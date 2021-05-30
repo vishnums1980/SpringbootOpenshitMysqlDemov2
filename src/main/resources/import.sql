705c5347aef68e9c50d1307883f98b6e739b82b6
leeyeekee <leeyeekee@hotmail.com>
2021-05-30 21:01:41 +0800

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into student (id, address, name) values (1, '2 Street 5 Garden Plaza', 'Martin');
+insert into student (id, address, name) values (2, '4 Street 7 Happy Garden', 'Zoe');
+insert into student (id, address, name) values (3, '6 District 5', 'Ernest');
