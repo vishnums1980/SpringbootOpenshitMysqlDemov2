52facb204130f1cde902a2f7d4ada9a152b53774
leeyeekee <leeyeekee@hotmail.com>
2021-06-13 11:53:40 +0800

    Update StudentController.java

--- src/main/java/com/codecraftshop/openshift/mysql/controller/StudentController.java
+++ src/main/java/com/codecraftshop/openshift/mysql/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.codecraftshop.openshift.mysql.controller;
 
 import java.util.List;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
